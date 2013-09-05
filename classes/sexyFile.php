54fec9b4c215fb6a74f0b39a60ebf2de426dcf9d
Cho Hwechul <flrngel@gmail.com>
2013-09-05 10:08:07 +0900

    Update sexyFile.php

--- classes/sexyFile.php
+++ classes/sexyFile.php
@@ -1,54 +1,64 @@
 <?
 class sexyFile extends sexyModel{
-	var $db;
-	var $dir;
-	var $files;
-	var $abspath="/dotname/htdata/dotname/";
-	var $table="files";
-	var $table_perm=array("filename","realname");
+        var $db;
+        var $dir;
+        var $files;
+        var $abspath=""; // path here
+        var $table="files";
+        var $table_perm=array("filename","realname");
 
-	function __construct($subpath){
-		$this->db=new DB\Mysql;
-		$this->files=array();
-		$this->dir=$subpath."/";
-	}
+        function __construct($subpath){
+                $this->db=new DB\Mysql;
+                $this->files=array();
+                $this->dir=$subpath."/";
+        }
 
-	private function toString(){
-		return implode("|",$this->files);
-	}
+        private function toString(){
+                return implode("|",$this->files);
+        }
 
-	function toArray($str){
-		return explode("|",$str);
-	}
+        function toArray($str){
+                return explode("|",$str);
+        }
 
-	function _upload($tmp_name,$realname){
-		if( !file_exists( $tmp_name ) ) return false;
-		$filename=sha1(uniqid());
-		$target=$this->dir.$filename;
-		$filepath=$this->abspath.$target;
-		echo $filepath;
-		if( move_uploaded_file($tmp_name,$filepath) ){
-		}
-		print_r(array("filename"=>$target,"realname"=>$realname));
-		$this->_insert($this->table_perm,array("filename"=>$target,"realname"=>$realname));
-		return $target;
-	}
+        function _upload($tmp_name,$realname){
+                if( !file_exists( $tmp_name ) ) return false;
+                $filename=sha1(uniqid());
+                $target=$this->dir.$filename;
+                $filepath=$this->abspath.$target;
+                echo $filepath;
+                if( move_uploaded_file($tmp_name,$filepath) ){
+                }
+                print_r(array("filename"=>$target,"realname"=>$realname));
+                $this->_insert($this->table_perm,array("filename"=>$target,"realname"=>$realname));
+                return $target;
+        }
 
-	function upload($var){
-		if( is_array($var) ){
-			foreach( $var as $key=>$val ){
-				$res=$this->_upload($val);
-				if( $res ){
-					$this->files[]=$res;
-				}
-			}
-		}else{
-			$res=$this->_upload($var);
-			if( $res ){
-				$this->files[]=$res;
-			}
-		}
-		return $this->toString();
-	}
+        function upload($files){
+                print_r($files);
+                if( is_array($files['tmp_name']) ){
+                        $var=array();
+                        foreach( $files['tmp_name'] as $key => $val ){
+                                $var[]=(object)array("tmp_name"=>$files['tmp_name'][$key],"realname"=>$files['name'][$key]);
+                        }
+                }else{
+                        $var=(object)array("tmp_name"=>$files['tmp_name'],"realname"=>$files['name']);
+                }
+
+                if( is_array($var) ){
+                        foreach( $var as $val ){
+                                $res=$this->_upload($val->tmp_name,$val->realname);
+                                if( $res ){
+                                        $this->files[]=$res;
+                                }
+                        }
+                }else{
+                        $res=$this->_upload($var->tmp_name,$var->realname);
+                        if( $res ){
+                                $this->files[]=$res;
+                        }
+                }
+                return $this->toString();
+        }
 }
 ?>
